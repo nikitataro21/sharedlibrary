74e26cf5dfa65bb93a60f27bc2b7150f3dd85be0
nikitataro21 <127369625+nikitataro21@users.noreply.github.com>
2023-03-20 18:22:30 +0530

    Update cidcd.groovy

--- var/cidcd.groovy
+++ var/cidcd.groovy
@@ -4,6 +4,13 @@ def newDownload(repo)
 }
 def newBuild()
 {
-  sh 'mvn package'
+  sh "mvn package"
 }
-
+def newDeploy(jobname,ip,appname
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def newTest(jobname1)
+{
+  sh "java -jar  /var/lib/jenkins/workspace/${jobname1}/testing.jar"  
+}
